bded82980556b792355e8fee6b69a9be2b362760
PshenAI <77882289+PshenAI@users.noreply.github.com>
2021-10-12 14:56:17 +0300

    Update FileComparator.java

--- tasks/fileComparator/FileComparator.java
+++ tasks/fileComparator/FileComparator.java
@@ -11,12 +11,23 @@ public class FileComparator {
 	public void fileComparator(File a, File b) throws IOException {
 		try (InputStream is = new FileInputStream(a); InputStream isa = new FileInputStream(b)) {
 
-			int temp0 = is.available();
-			byte[] isArr = new byte[temp0];
-			int temp1 = isa.available();
-			byte[] isaArr = new byte[temp1];
+			int count = 0;
+			int check = 0;
+			int gen = 0;
+			for (; gen != -1;) {
+				byte[] isArr = new byte[1000000];
+				gen = is.read(isArr);
 
-			if (Arrays.equals(isArr, isaArr)) {
+				byte[] isaArr = new byte[1000000];
+				gen = isa.read(isaArr);
+
+				count += 1;
+				if (Arrays.equals(isArr, isaArr)) {
+					check += 1;
+				}
+			}
+
+			if (count == check) {
 				System.out.println("Files are identical!");
 			} else {
 				System.out.println("Files are NOT identical!");
