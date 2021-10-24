b88ade0b2f1565c0d1efbfcea5fc42c692e8f1c6
PshenAI <antonvoloshyn25@gmail.com>
2021-10-24 13:20:33 +0300

    Generics&CollectionsDz

--- tasks/testList/Runner.java
+++ tasks/testList/Runner.java
@@ -2,31 +2,26 @@ package testList;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Runner {
 
 	public static void main(String[] args) {
+		Integer[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		List<Integer> list = new ArrayList<>(testListCreator(arr));
+	}
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter desired size for your list:");
-		int size = sc.nextInt();
-
-		List<Integer> testList = new ArrayList<>(size);
-
-		for (int i = 0; i < size; i++) {
-			testList.add((int) (Math.random() * size));
+	public static <E> List<E> testListCreator(E[] array) {
+		List<E> testList = new ArrayList<>(array.length);
+		for (int i = 0; i < array.length && i < 9; i++) {
+			testList.add(array[i]);
 		}
-
-		System.out.println(testList);
-
 		testList.remove(0);
 		testList.remove(0);
 		testList.remove(testList.size() - 1);
-
 		System.out.println("--------------------------------");
 
 		System.out.println(testList);
+		return testList;
 	}
 
 }
